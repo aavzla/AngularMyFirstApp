1c4f54932dc1e8d509be6a291ff87b21c6a1e0e7
AAvilan <andres.avilan@nurun.com>
2020-04-20 16:18:58 -0400

    Property Binding - Add a bind to the TypeScript property in our class to the DOM Node object property. Adding a behavior to the button so it will be enabled after two seconds.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', //By html element
   //selector: '[app-servers]',  //By html attribute
   //selector: '.app-servers', //By CSS class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    `,
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>
+  //  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
